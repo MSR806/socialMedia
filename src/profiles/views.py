5e9b8158d2d047807191a459651eb4d4f78b3fc8
MSR806 <msr0806@users.noreply.github.com>
2021-01-12 14:08:29 +0530

    navbar included in base

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileModelForm
 # Create your views here.
 
 def my_profile_view(request):
     profile = Profile.objects.get(user = request.user)
 
+    form = ProfileModelForm
+
     context = {
         'profile' : profile,
+        'form' : form
     }
 
     return render(request, 'profiles/myprofile.html', context)
